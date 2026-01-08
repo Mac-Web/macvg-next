b0dac08dc8751d14a0a6ec70c1a9d51108b17216
tonymac129 <tonyforself@gmail.com>
2026-01-07 22:29:43 -0500

    Set up repository structure, basic pages, layout, and components

---
+++ components/layout/Nav.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import PrimaryButton from "../ui/PrimaryButton";
+import Link from "next/link";
+import Image from "next/image";
+
+const navLinkStyles = "text-gray-800 dark:text-gray-300 transition-colors p-2.5 hover:text-orange-400";
+
+function Nav() {
+  function handleSignin() {
+    console.log("Sign in");
+  }
+
+  return (
+    <nav
+      className="flex items-center justify-between gap-x-2 border-b border-gray-700 px-5 md:px-20 lg:px-[calc(50%-550px)] h-17 z-5 
+    sticky top-0 bg-gray-200 dark:bg-gray-950"
+    >
+      <Link
+        href="/"
+        className="flex items-center gap-x-2 text-black dark:text-white text-lg duration-300 pr-5 py-2 font-bold
+       hover:text-shadow-gray-400 hover:text-shadow-sm"
+      >
+        <Image src="/logo.png" alt="MacWeb Logo" width={35} height={35} /> MacVG
+      </Link>
+      <div className="flex gap-x-3 items-center">
+        <div className="md:flex items-center gap-x-3 hidden">
+          <Link href="/new" className={navLinkStyles}>
+            New
+          </Link>
+          <Link href="/trending" className={navLinkStyles}>
+            Trending
+          </Link>
+          <Link href="/recent" className={navLinkStyles}>
+            Recent
+          </Link>
+          <Link href="/settings" className={navLinkStyles}>
+            Settings
+          </Link>
+        </div>
+        <PrimaryButton text="Sign in" click={handleSignin} />
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ components/ui/PrimaryButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+type PrimaryButtonProps = {
+  text: string;
+  click: () => void;
+};
+
+function PrimaryButton({ text, click }: PrimaryButtonProps) {
+  return (
+    <button
+      className="bg-orange-400 hover:bg-orange-500 duration-300 font-bold cursor-pointer rounded text-white py-1.5 px-4 text-lg"
+      onClick={() => click()}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default PrimaryButton;
